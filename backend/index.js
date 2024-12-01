774a8c080d732893429585e2ee20701e660a396c
Rahul Gupta <154514374+rahulguptahotmail@users.noreply.github.com>
2024-12-01 14:23:19 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'https://ecommerce-demoproject.vercel.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 // Routes
 const userRouter = require("./routes/user.routes");
 const productRouter = require("./routes/product.routes");
