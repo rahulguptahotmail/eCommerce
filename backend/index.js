569b724adeae4898ac0c736ff74b1f07ef264707
Rahul Gupta DevOps <154514374+rahulguptadevops@users.noreply.github.com>
2026-04-21 03:39:27 -0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'https://ecommerce-demoproject.vercel.app');
+  res.setHeader('Access-Control-Allow-Origin', 'https://ecommerce-rahulguptadevops.vercel.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', true);
